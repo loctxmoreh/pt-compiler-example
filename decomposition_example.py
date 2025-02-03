6348f57233df61fc56846bb6c8e930877635868b
Tran Xuan Loc <loc.tran@moreh.com.vn>
2025-02-03 08:12:30 +0000

    manual silu decomposition

--- decomposition_example.py
+++ decomposition_example.py
@@ -34,7 +34,37 @@ def simple_silu_decomp(gm, example_inputs):
 
 
 def manual_silu_decomp(gm, example_inputs):
-    pass
+    assert "silu" in gm.code
+
+    def _silu_decomposer(gm, example_inputs):
+        new_graph = torch.fx.Graph()
+        env = {}
+        tracer = torch.fx.proxy.GraphAppendingTracer(new_graph)
+
+        for node in gm.graph.nodes:
+            if node.op == "call_function" and node.target == torch.ops.aten.silu.default:
+                proxy_args = [
+                    torch.fx.Proxy(env[x.name], tracer) if isinstance(x, torch.fx.Node) else x
+                    for x in node.args
+                ]
+                output_proxy = _silu_decomposition(*proxy_args)
+
+                new_node = output_proxy.node
+                env[node.name] = new_node
+            else:
+                new_node = new_graph.node_copy(node, lambda n: env[n.name])
+                env[node.name] = new_node
+
+        new_graph.lint()
+        new_gm = torch.fx.GraphModule(gm, new_graph)
+        new_gm.recompile()
+        assert "silu" not in new_gm.code
+        return new_gm
+
+    return aot_module_simplified(
+        gm, example_inputs,
+        fw_compiler=_silu_decomposer,
+    )
 
 
 def main():
@@ -52,7 +82,9 @@ def main():
 
     with torch.inference_mode():
         torch._dynamo.reset()
-        fn = torch.compile(backend=simple_silu_decomp)(model)
+        backend = simple_silu_decomp
+        backend = manual_silu_decomp
+        fn = torch.compile(backend=backend)(model)
         out = fn(input_)
 
     torch.testing.assert_close(out, expected)
